4c33dbe7dd2077e71a0daa04f240027c80211ae4
MarlonGaitan <89613522+MarlonGaitan@users.noreply.github.com>
2024-04-20 15:23:17 -0500

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp, getApps } from "firebase/app"
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyAn5dJ7rtIH_7fLQYlIwKHl1xQs7FkV81Y",
+    authDomain: "brew-its.firebaseapp.com",
+    databaseURL: "https://brew-its-default-rtdb.firebaseio.com",
+    projectId: "brew-its",
+    storageBucket: "brew-its.appspot.com",
+    messagingSenderId: "294351439178",
+    appId: "1:294351439178:web:f02d78f9f4488495eeadb3",
+    measurementId: "G-HDYTT02RDR"
+}
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+export {db}
