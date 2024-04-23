f282eabd761db5a1f9e45f38415e0b0dd56ad1e8
ariyam12 <juanalucho9@gmail.com>
2024-04-22 19:47:58 -0500

    Dropbox fixed on the Create View. Create ticket now navigates to the corresponding view.

--- components/DropboxComp.vue
+++ components/DropboxComp.vue
@@ -7,12 +7,12 @@
                 width="30" height="30" 
                 viewBox="0 0 30 30" 
                 id="down-drop-arrow"
-                class="inline-block ml-2 -mr-1">
+                class="inline-block ml-2 -mr-1 right-0">
                 <path d="M8.71 11.71l2.59 2.59c.39.39 1.02.39 1.41 0l2.59-2.59c.63-.63.18-1.71-.71-1.71H9.41c-.89 0-1.33 1.08-.7 1.71z"></path>
             </svg>
             <!-- Close button -->
         </button>
-        <button v-if="selected" @click="clearOption()" class="absolute -mt-2 -ml-2 top-0 left-0 flex rounded-full justify-center items-center bg-custom-red-wine">
+        <button v-if="selected && has_clear_button" @click="clearOption()" class="absolute -mt-2 -ml-2 top-0 left-0 flex rounded-full justify-center items-center bg-custom-red-wine">
             <svg width="15px" height="15px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="none">
                 <path stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 23.5l15-15M23.5 23.5l-15-15"/>
             </svg>
@@ -39,7 +39,7 @@
 
 <script>
 export default {
-    props: ['options','text_type','default_caption','type']
+    props: ['options','text_type','default_caption','type', 'has_clear_button']
     , 
     components: {
     },
