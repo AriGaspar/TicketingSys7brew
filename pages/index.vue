009b47351e515a72acca4b37677e02044ec32e3a
robPTY <rcadown8@gmail.com>
2024-04-23 21:44:56 -0500

    added extra styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,35 @@
 <template>
-    <section class="container">
-        <div>
-            <h2>
-                Write to Firestore.
-            </h2>
-            <div>
-                <button @click.native="writeUserData">
-                    <span>Write now</span>
-                </button>
-            </div>
-        </div>
-    </section>
+  <section class="container">
+    <div>
+      <h2>Write to Firestore.</h2>
+      <div>
+        <button @click.native="writeUserData">
+          <span>Write now</span>
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-import { db } from "../firebaseConfig.js"
+import { db } from "../firebaseConfig.js";
 import { ref, set } from "firebase/database";
-export default{
-methods:{
-writeUserData:function() {
-    const reference = ref(db, "users/user2/");
-    set(reference,{
-        username:"Marlon"
-    });
-}
-}
-}
+export default {
+  methods: {
+    writeUserData: function () {
+      const reference = ref(db, "users/user2/");
+      set(reference, {
+        username: "Marlon",
+      });
+    },
+  },
+};
 </script>
 <style>
 .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 </style>
